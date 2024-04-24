ff502f164c3bc6d54bd68da882f25d2cfc250af1
test user <test@test.com>
2024-04-24 08:22:20 -0500

    added two new files to the company package CompanyRepository and CompanyService to manage repository and a new package for testing companyTest and the file companyRepositoryTest. Also edited the .gitIgnore to avoid push the generated effective-pom.xml

--- src/main/java/edu/site/jobBook/Company/Company.java
+++ src/main/java/edu/site/jobBook/Company/Company.java
@@ -1,19 +1,29 @@
-package edu.site.jobBook.Company;
+package edu.site.jobBook.company;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import jakarta.persistence.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
+@Table(name = "companies")
 public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String Name;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "description", length = 255)
     private String description;
+    //waiting on other classes to be more developed
     // private List<Post> posts;
-    //private List<Job> jobs;
+    // private List<Job> jobs;
 }
---
+++ src/main/java/edu/site/jobBook/Company/CompanyRepository.java
@@ -0,0 +1,10 @@
+package edu.site.jobBook.company;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CompanyRepository extends CrudRepository<Company, Long>{
+    //https://docs.spring.io/spring-data/jpa/docs/1.5.1.RELEASE/reference/html/jpa.repositories.html#jpa.query-methods.query-creation
+    public Optional<Company> findBynameIgnoreCase(String name); //i get it but dont get it.
+}
