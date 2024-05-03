fb6e033fe979ecf84a6c65c1a26f0297e0982f7d
Dillon-777G <d777g@protonmail.com>
2024-05-03 15:24:25 -0700

    Package info, Rollback redundancy

--- src/main/java/edu/site/jobBook/user/User.java
+++ src/main/java/edu/site/jobBook/user/User.java
@@ -10,16 +10,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-/**
- * Utilizes Lombok to simplify Java code for entity modeling
- * Features demonstrated:
- * @Data - Automatically generates getters, setters, toString, equals, and hashCode methods
- * @NoArgsConstructor - Generates a no-argument constructor
- * @AllArgsConstructor - Generates a constructor initializing all fields
- * @Builder - Implements the Builder pattern for object creation
- * @Entity - This annotation is required to specify the class as a JPA entity
- * @Table - This maps to the specific column needed in the database
- */
+
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/edu/site/jobBook/user/package-info.java
+++ src/main/java/edu/site/jobBook/user/package-info.java
@@ -1,25 +1,31 @@
-/**
- * Lombok usage
- * <ul>
- * <li>@Data for clean and clear POJOs
- * <li>@NoArgsConstructor, @AllArgsConstructor for flexible instantiation
- * <li>@Builder to support the Builder pattern
- * </ul>
- * 
- * Jakarta 
- * <ul>
- * <li>@Id to specify the primary key of the entity
- * <li>@GeneratedValue to define the strategy for generating primary key values
- * <li>@Embeddable to embed the profile into the user entity
- * <li>@Table for specifying the table name for persistence
- * <li>@Entity to mark the class as a persistence entity
- * </ul>
- * 
- * Spring Repository
- * <ul>
- * <li>@Repository to indicate the class serves as a repository for data access
- * </ul>
- * 
- * @author Dillon
- */
+/*********************************************************************************
+ * The follwing document descibes how the Springboot                             *
+ * framework, jakarta, and lombok are leveraged to create                        *
+ * persistence for the user class.                                               *
+ * *******************************************************************************
+ *                                                                               *
+ * lombok                                                                        *
+ * <ul>                                                                          *
+ * <li>@Data for clean and clear POJOs                                           *
+ * <li>@NoArgsConstructor, @AllArgsConstructor for flexible instantiation        *
+ * <li>@Builder to support the Builder pattern                                   *
+ * </ul>                                                                         *
+ *                                                                               *
+ *                                                                               *
+ * jakarta                                                                       *
+ * <ul>                                                                          *
+ * <li>@Id to specify the primary key of the entity                              *
+ * <li>@GeneratedValue to define the strategy for generating primary key values  *
+ * <li>@Embeddable to embed the profile into the user entity                     *
+ * <li>@Table for specifying the table name for persistence                      *
+ * <li>@Entity to mark the class as a persistence entity                         *
+ * </ul>                                                                         *
+ *                                                                               *
+ * Spring Repository                                                             *
+ * <ul>                                                                          *
+ * <li>@Repository to indicate the class serves as a repository for data access  *
+ * </ul>                                                                         *
+ *                                                                               *
+ * @author Dillon                                                                *
+ *********************************************************************************/
 package edu.site.jobBook.user;
