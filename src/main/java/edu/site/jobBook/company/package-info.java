05b1211c924dccbe0c8aa470baf63fb8799f589b
test user <test@test.com>
2024-05-06 07:45:56 -0500

    added more details to package info

--- src/main/java/edu/site/jobBook/company/package-info.java
+++ src/main/java/edu/site/jobBook/company/package-info.java
@@ -7,6 +7,8 @@
  * <b> Package File update for Milestone 2 for SE452
  * <ul>
  * <li> Company.java - An object that represents a company such as name, description, jobs that it can provides and posts that are made
+ * <li> One To Many Company to Jobs
+ * <li> One To Many Company to Posts
  * <li> CompanyRepository.java - Handling the CRUD operations for a company 
  * <li> CompanyService.java - The buisiness logic gor acompany
  * <li> CompanyController.java - provides endpoints for company logic.
