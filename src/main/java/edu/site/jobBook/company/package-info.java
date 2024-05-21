722eccbf7115502e5f55f82111fb1fd9d27614a5
Dillon-777G <d777g@protonmail.com>
2024-05-21 14:50:07 -0700

    Admin session

---
+++ src/main/java/edu/site/jobBook/company/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * <b> Package File for Milestone 1 for SE452
+ * <ul>
+ * <li> Defining the Company object for JobBook application
+ * <li> Its intended to represent a company information and such as name, about, available jobs. 
+ * <ul>
+ * <b> Package File update for Milestone 2 for SE452
+ * <ul>
+ * <li> Company.java - An object that represents a company such as name, description, jobs that it can provides and posts that are made
+ * <li> One To Many Company to Jobs
+ * <li> One To Many Company to Posts
+ * <li> CompanyRepository.java - Handling the CRUD operations for a company 
+ * <li> CompanyService.java - The buisiness logic gor acompany
+ * <li> CompanyController.java - provides endpoints for company logic.
+ * @author Alexander Lazarov
+ */
+package edu.site.jobBook.company;
---
+++ src/main/java/edu/site/jobBook/job/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * <p>
+ * Package containing classes related to job management.
+ * It includes entities representing job details, job applications, job statuses,
+ * controllers for handling job-related requests, services for business logic,
+ * and repositories for database interactions.
+ *</p>
+ * This package also demonstrates the use of Lombok for handling Job data in the job registration service.
+ * <ul>
+ * <li>Utilizes Lombok annotations for clean and clear POJOs
+ * <li>Features @Data for automatic generation of getters, setters, equals, hashCode, and toString methods
+ * <li>Includes @NoArgsConstructor, @AllArgsConstructor for flexible instantiation
+ * <li>Employs @Builder to support the Builder pattern for easy object creation
+ * </ul>
+ * These features help minimize boilerplate code and improve clarity and maintainability.
+ * 
+ * @author Ravi Dhondkar
+ * @version 1.0
+ */
+package edu.site.jobBook.job;
