840a8fb03e869d8d23f03649c8f290ba09db7ecf
test user <test@test.com>
2024-05-30 17:04:53 -0500

    updated package info for company and hiringstatus. Also cleaned some unused code.

--- src/main/java/edu/site/jobBook/company/HiringStatus/CompanyStatusInitializer.java
+++ src/main/java/edu/site/jobBook/company/HiringStatus/CompanyStatusInitializer.java
@@ -1,12 +1,6 @@
 package edu.site.jobBook.company.HiringStatus;
 
-import java.util.ArrayList;
-
-import edu.site.jobBook.company.Company;
 import edu.site.jobBook.company.CompanyRepository;
-import edu.site.jobBook.company.HiringStatus.CompanyHiringStatus;
-import edu.site.jobBook.company.HiringStatus.HiringStatus;
-import edu.site.jobBook.company.HiringStatus.HiringStatusRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
--- src/main/java/edu/site/jobBook/company/package-info.java
+++ src/main/java/edu/site/jobBook/company/package-info.java
@@ -12,6 +12,13 @@
  * <li> CompanyRepository.java - Handling the CRUD operations for a company 
  * <li> CompanyService.java - The buisiness logic gor acompany
  * <li> CompanyController.java - provides endpoints for company logic.
+ * <li>
+ * <li> Package File update for Milestone 3 for SE452
+ * <li>
+ * <li> CompanyUIController.java - Backend endpoints for users to navigate the company page UI.
+ * <li> compananies-page.html - bootstrap and thmye static webpage for user to to navigate through the list of companiess
+ * <li> companiy-details.html -bootstrap and thyme static webpage for user to view a specific companies detais and access job offerings and posts.
+ * 
  * @author Alexander Lazarov
  */
 package edu.site.jobBook.company;
